4970d5709e19cc1c19358a408ddd8896c3c0b6da
“Awanee <Awanee.pandey@gmail.com>
2021-02-01 17:32:18 -0600

    smooth scrolling

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click',openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/* Smooth Scrolling */
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',e=>{
+  const s1coords = section1.getBoundingClientRect();
+  console.log(e.target.getBoundingClientRect());
+  console.log('current scroll (x/y)',window.pageXOffset,window.pageYOffset);
+ 
+ /* Old way */
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior:'smooth',
+  })
+
+  /* New way */
+  section1.scrollIntoView({behavior: 'smooth'});
+
+})
+
+/* Smooth scrolling for navbar */
+const logo = document.querySelector('.nav__logo');
+const container = document.querySelector('.nav__links');
+
+container.addEventListener('click', e => {
+  e.preventDefault();
+/* Guard clause */
+if(!e.target.classList.contains('nav__link')) return;
+
+const href = e.target.getAttribute('href');
+console.log(href);
+document.querySelector(href).scrollIntoView({behavior:'smooth'});
+});
